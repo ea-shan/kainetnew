463743bb8ac953be6097af7fb71561db69275323
ea-shan <shantanu.goswami@expressanalytics.net>
2026-08-27 17:52:50 +0530

    Workflow section design fixes

--- src/components/sites/www-twelvelabs-io-a368af44/root-8a5edab2/TrustedBySection.tsx
+++ src/components/sites/www-twelvelabs-io-a368af44/root-8a5edab2/TrustedBySection.tsx
@@ -4,14 +4,14 @@ export function TrustedBySection() {
   const logos = [...partnerLogos, ...partnerLogos];
   return (
     <section className="overflow-hidden bg-[#EEEEEE] py-16 text-[#000000]">
-      <p className="mb-8 text-center text-[12px] tracking-[0.18em] text-[#000000]/50">TRUSTED BY</p>
+      {/* <p className="mb-8 text-center text-[12px] tracking-[0.18em] text-[#000000]/50">TRUSTED BY</p>
       <div className="relative">
         <div className="tl-marquee flex w-max items-center gap-14 px-8">
           {logos.map((src, i) => (
             <img key={`${src}-${i}`} src={src} alt="" className="h-7 w-auto opacity-80" />
           ))}
         </div>
-      </div>
+      </div> */}
     </section>
   );
 }
